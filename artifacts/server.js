a8d6c98d3269e18bf3737f14213c0432d0d85680
Reshma Ajith <reshmaae@gmail.com>
2025-05-11 23:49:41 +0530

    test

--- artifacts/server.js
+++ artifacts/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-var path = require('path');        
-const port = process.env.PORT ||3000;   
+var path = require('path');
+const port = process.env.PORT ||3000;
 const app = express();
 
 //Set the base path to the angular-test dist folder
-app.use(express.static(path.join(__dirname, 'dist/sample-project-angular-one')));
+app.use(express.static(path.join(__dirname, 'dist/sample-angular-project-three/browser')));
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/sample-project-angular-one/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/sample-angular-project-three/browser/index.html'));
 });
 
 //Starting server on port 8081
